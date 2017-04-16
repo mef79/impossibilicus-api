087bdc275485de409dbfe36f763ac84cc7468c4d
Meaghan <mefenelon@gmail.com>
2017-04-15 20:45:23 -0400

    Allow all cross-origin requests until there's a better way to handle them

--- app.py
+++ app.py
@@ -5,19 +5,20 @@ from pymongo import MongoClient
 from flask_restful import Api
 from dao import Dao
 from resources import *
+from flask_cors import CORS
 
 # imports from other files
 from errors import NotFoundError, UpdateError
 from resources import Home, Story, StoryList
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 uri = os.getenv('MONGODB_URI')
 username = os.getenv('MONGO_USERNAME')
 client = MongoClient(uri)
 story_collection = client[username].story
 dao = Dao(story_collection)
-
 
 api.add_resource(Home, '/')
 api.add_resource(Story, '/story', '/story/<string:name>',
