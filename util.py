4a5c7b91a501182150ad9840ed5f1d885d27a4f5
Meaghan <mefenelon@gmail.com>
2017-06-04 23:13:35 -0400

    Add validation for node and link counters

--- util.py
+++ util.py
@@ -2,6 +2,8 @@ def validate(data):
 	assert 'name' in data
 	assert 'nodes' in data
 	assert 'links' in data
+	assert 'node_counter' in data
+	assert 'link_counter' in data
 	return data
 
 def error(message):
