b8f8d67f8b3ab2fd448f346dc3ad9fff3138ab09
Meaghan <mefenelon@gmail.com>
2017-06-11 19:48:27 -0400

    Friendlier error messages on startup failure for missing env variables

--- app.py
+++ app.py
@@ -10,21 +10,48 @@ from flask_cors import CORS
 # imports from other files
 from errors import NotFoundError, UpdateError
 from resources import Home, Story, StoryList
+from util import Timeout
 
-app = Flask(__name__)
-CORS(app)
-api = Api(app)
-uri = os.getenv('MONGODB_URI')
-username = os.getenv('MONGO_USERNAME')
-client = MongoClient(uri)
-story_collection = client[username].story
-dao = Dao(story_collection)
+def main():
+	app = Flask(__name__)
 
-api.add_resource(Home, '/')
-api.add_resource(Story, '/story', '/story/<string:name>',
-	resource_class_kwargs={'dao':dao})
-api.add_resource(StoryList, '/stories',
-	resource_class_kwargs={'dao':dao})
+	# enable cross-origin requests
+	CORS(app)
+
+	# initialize the flask-api library
+	api = Api(app)
+
+	# initialize the mongo connection string variables
+	uri = os.getenv('MONGODB_URI')
+	username = os.getenv('MONGO_USERNAME')
+	client = MongoClient(uri)
+
+	# attempt to initialize story collection
+	try:
+		story_collection = client[username].story
+	except TypeError:
+		print ('Must enter Mongo username environment variable MONGO_USERNAME')
+		print ('Failed to start')
+		return
+
+	try:
+		with Timeout(3):
+			client.admin.command('ismaster')
+	except Timeout.Timeout:
+		print ('Connection to MongoDB failed, check MONGODB_URI environment variable')
+		print ('Failed to start')
+		return
+
+	dao = Dao(story_collection)
+
+	api.add_resource(Home, '/')
+	api.add_resource(Story, '/story', '/story/<string:name>',
+		resource_class_kwargs={'dao':dao})
+	api.add_resource(StoryList, '/stories',
+		resource_class_kwargs={'dao':dao})
+	api.add_resource(StoryImport, '/storyImport',
+		resource_class_kwargs={'dao':dao})
+	app.run()
 
 if __name__ == '__main__':
-	app.run()
+	main()
