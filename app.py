23ff48bea497acc9e44735b11dbe249fbc27ceae
Meaghan <mefenelon@gmail.com>
2017-06-20 21:47:34 -0400

    Try harder to fix gunicorn

--- app.py
+++ app.py
@@ -14,44 +14,40 @@ from util import Timeout
 
 app = Flask(__name__)
 
-def main():
-	# enable cross-origin requests
-	CORS(app)
+# enable cross-origin requests
+CORS(app)
 
-	# initialize the flask-api library
-	api = Api(app)
+# initialize the flask-api library
+api = Api(app)
 
-	# initialize the mongo connection string variables
-	uri = os.getenv('MONGODB_URI')
-	username = os.getenv('MONGO_USERNAME')
-	client = MongoClient(uri)
+# initialize the mongo connection string variables
+uri = os.getenv('MONGODB_URI')
+username = os.getenv('MONGO_USERNAME')
+client = MongoClient(uri)
 
-	# attempt to initialize story collection
-	try:
-		story_collection = client[username].story
-	except TypeError:
-		print ('Must enter Mongo username environment variable MONGO_USERNAME')
-		print ('Failed to start')
-		return
+# attempt to initialize story collection
+try:
+	story_collection = client[username].story
+except TypeError:
+	print ('Must enter Mongo username environment variable MONGO_USERNAME')
+	print ('Failed to start')
+	return
 
-	try:
-		with Timeout(5):
-			client.admin.command('ismaster')
-	except Timeout.Timeout:
-		print ('Connection to MongoDB failed, check MONGODB_URI environment variable')
-		print ('Failed to start')
-		return
+try:
+	with Timeout(5):
+		client.admin.command('ismaster')
+except Timeout.Timeout:
+	print ('Connection to MongoDB failed, check MONGODB_URI environment variable')
+	print ('Failed to start')
+	return
 
-	dao = Dao(story_collection)
+dao = Dao(story_collection)
 
-	api.add_resource(Home, '/')
-	api.add_resource(Story, '/story', '/story/<string:name>',
-		resource_class_kwargs={'dao':dao})
-	api.add_resource(StoryList, '/stories',
-		resource_class_kwargs={'dao':dao})
-	api.add_resource(StoryImport, '/storyImport/<string:name>',
-		resource_class_kwargs={'dao':dao})
-	app.run()
-
-if __name__ == '__main__':
-	main()
+api.add_resource(Home, '/')
+api.add_resource(Story, '/story', '/story/<string:name>',
+	resource_class_kwargs={'dao':dao})
+api.add_resource(StoryList, '/stories',
+	resource_class_kwargs={'dao':dao})
+api.add_resource(StoryImport, '/storyImport/<string:name>',
+	resource_class_kwargs={'dao':dao})
+app.run()
